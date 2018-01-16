7cdccbaa94b8bdf5616bd0013da3f1a67072a7ee
Mikhail Klimenko <mk@moviestarplanet.com>
2018-01-16 16:25:14 +0100

    Initial commit

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+
+    public GameObject playerPrefab;
+
+    [HideInInspector]
+    public Vector2 playerRespawnPoint;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.MakePlayer();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void MakePlayer()
+    {
+        var startPoint = FindObjectOfType<PlayerSpawn>();
+        if (startPoint != null)
+        {
+            playerRespawnPoint = startPoint.transform.position;
+            //startPoint.LevelStarted();
+        }
+        Instantiate(playerPrefab, playerRespawnPoint, playerPrefab.transform.rotation);
+    }
+}
+
