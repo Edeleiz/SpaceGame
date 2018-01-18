70dd70b6ed950a887fee7cd1b26934fcdee941dc
Mikhail Klimenko <mk@moviestarplanet.com>
2018-01-18 13:16:45 +0100

    Updated movement system + animations

--- Assets/Scripts/Components/Moving/Behaviours/BaseMovingBehaviour.cs
+++ Assets/Scripts/Components/Moving/Behaviours/BaseMovingBehaviour.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseMovingBehaviour : MonoBehaviour
+public class BaseMovingBehaviour : UnityEngine.ScriptableObject
 {
     protected static void BasicMovement(Rigidbody2D target, Vector3 direction, float speed)
     {
-        var animator = target.GetComponent<Animator>();
+        var animator = target.GetComponentInChildren<Animator>();
         if (animator != null)
-            animator.SetBool("moving", direction.magnitude > 0);
+        {
+            animator.SetBool("is_moving", direction.magnitude > 0);
+            if (direction != Vector3.zero)
+            {
+                animator.SetFloat("input_x", direction.x);
+                animator.SetFloat("input_y", direction.y);
+            }
+        }
 
         target.velocity = Vector3.ClampMagnitude(direction, 1f) * speed;
         if (Mathf.Abs(direction.x) > 0)
         {
-            target.transform.localScale = direction.x < 0f ? new Vector3(-1f, 1f, 1f) : new Vector3(1f, 1f, 1f);
+            target.transform.localScale = direction.x < 0f ? new Vector3(1f, 1f, 1f) : new Vector3(-1f, 1f, 1f);
         }
     }
 
+    [HideInInspector]
     public bool isMoving = false;
 
+    [HideInInspector]
     public Rigidbody2D target;
 
     protected Vector3 _direction;
-
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
 	
 	// Update is called once per frame
 	public void Update ()
--- Assets/Scripts/Components/Moving/Behaviours/PersonNormalMoving.cs
+++ Assets/Scripts/Components/Moving/Behaviours/PersonNormalMoving.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "PersonMovingBehaviour", menuName = "My Components/Behaviours/Moving/Person", order = 1)]
 public class PersonNormalMoving : BaseMovingBehaviour
 {
     public float speed = 2.0f;
