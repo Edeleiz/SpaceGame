b5d736bcea34c97bad81f22d505c7dd8568ec0e3
Mikhail Klimenko <mk@moviestarplanet.com>
2018-01-22 16:29:36 +0100

    added base logic to inventory

--- Assets/Scripts/Components/Inventory/Inventory.cs
+++ Assets/Scripts/Components/Inventory/Inventory.cs
@@ -9,11 +9,13 @@ public class Inventory : MonoBehaviour
     [SerializeField]
     private ItemsDicitonary items;
 
+    private List<BaseItem> _itemsList;
+
     // Use this for initialization
     void Start ()
     {
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -23,31 +25,63 @@ public class Inventory : MonoBehaviour
 
     public bool AddItem(BaseItem item)
     {
+        AddItem(item, 1);
         return true;
     }
 
     public bool AddItem(BaseItem item, int count)
     {
+        items[item] += count;
+        _itemsList = null;
         return true;
     }
 
     public bool Removetem(BaseItem item)
     {
+        RemoveItem(item, 1);
         return true;
     }
 
     public bool RemoveItem(BaseItem item, int count)
     {
+        if (items.ContainsKey(item) == false)
+            return false;
+        items[item] -= count;
+        if (items[item] < 0)
+            items.Remove(item);
+        _itemsList = null;
         return true;
     }
 
-    public void Debug()
+    public int GetItemCount(BaseItem item)
     {
+        if (items.ContainsKey(item) == false)
+            return 0;
+        return items[item];
+    }
 
+    public List<BaseItem> GetItems()
+    {
+        if (_itemsList != null)
+            return _itemsList;
+
+        _itemsList = new List<BaseItem>();
+
+        foreach (var pair in items)
+        {
+            _itemsList.Add(pair.Key);
+        }
+
+        return _itemsList;
     }
 
     protected virtual void OnDestroy()
     {
-        
+        if (items != null)
+            items.Clear();
+        items = null;
+        if (_itemsList != null)
+            _itemsList.Clear();
+        _itemsList = null;
     }
 }
--- Assets/Scripts/Components/Inventory/Item/BaseItem.cs
+++ Assets/Scripts/Components/Inventory/Item/BaseItem.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Base Item", menuName = "My Components/Items/Base Item", order = 1)]
 public class BaseItem : ScriptableObject
 {
+    //Should be uniq name
     public string itemName;
 
     public bool isStackable;
+
+    public BaseItem Clone()
+    {
+        var clone = ScriptableObject.CreateInstance<BaseItem>();
+        clone.name = this.itemName;
+        clone.isStackable = this.isStackable;
+        return clone;
+    }
 }
