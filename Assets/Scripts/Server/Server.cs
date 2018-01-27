65b9560c337654463961fe4d631ff9926dc6b0f8
kolombet <anonrab@ya.ru>
2018-01-27 20:54:56 +0300

    Unity bug fix:  webRequest.chunkedTransfer = false;

--- Assets/Scripts/Server/Server.cs
+++ Assets/Scripts/Server/Server.cs
@@ -1,23 +1,16 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class Server: MonoBehaviour
 {
-    private void Start()
-    {
-        TestRequest();
-    }
-
     public void TestRequest()
     {
-//        LoadTreasureData(data =>
-//        {
-//            print("hello");
-//        });
-//        
+        LoadTreasureData(data =>
+        {
+            print("hello");
+        });
+        
         var testData = new TreasureData();
         testData.X = 100;
         testData.Y = 200;
@@ -32,13 +25,10 @@ public class Server: MonoBehaviour
 
     public const string SERVER_URL = "http://46.101.200.56:8080";
 //    public const string SERVER_URL = "http://localhost:3000";
+    
     public void SendTreasureData(TreasureData data, System.Action<bool> callback)
     {
-        StartCoroutine(PostRequest2(callback, data, SERVER_URL + "/sendPackage"));
-//        StartCoroutine(Upload(callback, data, SERVER_URL + "/sendPackage"));
-        
-        
-//        StartCoroutine(PostRequest(callback, data, SERVER_URL + "/sendPackage"));
+        StartCoroutine(Upload(callback, data, SERVER_URL + "/sendPackage"));
     }
 
     public void LoadTreasureData(System.Action<TreasureData> callback)
@@ -49,6 +39,7 @@ public class Server: MonoBehaviour
     private IEnumerator Load(System.Action<TreasureData> callback, string url)
     {
         ServerTreasureData data;
+        
         using (UnityWebRequest request = UnityWebRequest.Get(url))
         {
             yield return request.Send();
@@ -75,6 +66,7 @@ public class Server: MonoBehaviour
         formData.AddField("message", data.Message);
         
         UnityWebRequest www = UnityWebRequest.Post(url, formData);
+        www.chunkedTransfer = false;
         yield return www.SendWebRequest();
  
         if(www.isNetworkError || www.isHttpError) {
@@ -85,35 +77,5 @@ public class Server: MonoBehaviour
             Debug.Log("Form upload complete!");
             callback(true);
         }
-    }
-    
-    private IEnumerator PostRequest(System.Action<bool> callback, TreasureData data, string url)
-    {
-        var json = "{\"locationName\":\"Moscow\",\"x\":\"150\",\"y\":\"250\",\"message\":\"hello world\"}";
-        var request = new UnityWebRequest(url, "POST");
-        byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(json);
-        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        yield return request.Send();
-
-        Debug.Log("Response: " + request.downloadHandler.text);
-        callback(true);
-    }
-    
-    private IEnumerator PostRequest2(System.Action<bool> callback, TreasureData data, string url)
-    {
-        var json = "{\"locationName\":\"Moscow\",\"x\":\"150\",\"y\":\"250\",\"message\":\"hello world\"}";
-        var request = new UnityWebRequest(url, "POST");
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
-        request.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
- 
-        yield return request.Send();
- 
-        Debug.Log("Status Code: " + request.responseCode);
-        callback(true);
     }
 }
