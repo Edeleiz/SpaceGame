d191f259e71d81d6431236d4ae1cacbfd34e41f6
kolombet <anonrab@ya.ru>
2018-01-28 23:36:40 +0300

    ui update

--- Assets/Scripts/PhoneJunkie.cs
+++ Assets/Scripts/PhoneJunkie.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Phone : MonoBehaviour
+public class PhoneJunkie : MonoBehaviour
 {
     private bool isLoaded = false;
     void Update()
     {
-        if (Server.Instance != null && isLoaded == false)
+        gameObject.SetActive(SpicerGameManager.instance.gameMode == GameMode.Junkie);
+        
+        if (SpicerGameManager.instance.gameMode == GameMode.Junkie && !isLoaded && SpicerGameManager.instance.treasureData != null)
         {
-            Server.Instance.LoadTreasureData(data =>
-            {
-                isLoaded = true;
-                var tf = gameObject.GetComponent<Text>();
-                tf.text = data.Message;
-            });
+            isLoaded = true;
+            var m = SpicerGameManager.instance.treasureData.Message;
+            var tfObj = gameObject.transform.Find("ChildName");
+            tfObj.GetComponent<Text>().text = m;
         }
+//        if (Server.Instance != null && isLoaded == false)
+//        {
+//            Server.Instance.LoadTreasureData(data =>
+//            {
+//                isLoaded = true;
+//                var tf = gameObject.GetComponent<Text>();
+//                tf.text = data.Message;
+//            });
+//        }
     }
 }
